6f9e8d6e9530027f32fe60c5d664d89661f49a6f
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-29 22:18:56 +0300

    fill method showCredits()

--- src/com/gmail/aleksandrphilimonov/Starter.java
+++ src/com/gmail/aleksandrphilimonov/Starter.java
@@ -1,7 +1,50 @@
 package com.gmail.aleksandrphilimonov;
 
+import java.util.Scanner;
+
 public class Starter {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        String command;
+        do {
+            System.out.println("Welcome to SimpleJavaGame V1.0 \nPlease make your choice and press Enter: \n");
+            System.out.println("1: Start new game");
+            System.out.println("2: Options");
+            System.out.println("3: Credits");
+            System.out.println("4: Exit");
+
+            command = scanner.nextLine();
+
+            switch (command) {
+                case "1":
+                    startNewGame();
+                    break;
+
+                case "2":
+                    openOptionsMenu();
+                    break;
+
+                case "3":
+                    showCredits();
+                    break;
+
+                case "4":
+                    break;
+
+                default:
+                    System.out.println("Command not recognized! Please try again");
+            }
+        } while (!command.equals("4"));
+    }
+
+    private static void showCredits() {
+        System.out.println("\t\t----------------------------------------------------\n\t\t\tCreated by Aleksandr Philimonov\n\t\t\tVersion 1.0\n\t\t\tLast modified on 29.10.2022\n\t\t\tContact me at aleksandrphilimonov@gmail.com\n\t\t----------------------------------------------------\n");
+    }
+
+    private static void openOptionsMenu() {
+    }
+
+    private static void startNewGame() {
     }
 }
